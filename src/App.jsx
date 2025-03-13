554ae7eb210343944e26ef04bc63e73e9c553e28
Amanda Trinity <amandatrinity16@gmail.com>
2025-03-13 17:12:54 -0300

    :sparkles: add components to display taks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
@@ -14,14 +15,40 @@ function App() {
     title: 'Ir ao shopping',
     description: 'Comprar blusa vermelha',
     isCompleted: false,
+  },
+  {
+    id: 3,
+    title: 'Ir ao supermercado',
+    description: 'Comprar laranja',
+    isCompleted: false,
+  },
+  ]);
+
+  function onTaskClick (taskId) {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId){
+        return{... task,isCompleted: !task.isCompleted}
+      }
+
+      return task
+    })
+    setTasks(newTasks);
   }
-  ])
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
    return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-screen h-screen bg-purple-400 flex justify-center p-6">
       <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <h1 className="text-3xl text-white-800 font-bold text-center">Gerenciador de Tarefas</h1>
         <AddTask/>
-        <Tasks />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
